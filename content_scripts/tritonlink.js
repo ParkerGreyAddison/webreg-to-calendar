19faee75cd9c1fbaa179fbbccc41c6295adf9326
Parker Grey Addison <pargrey@gmail.com>
2018-09-09 18:44:59 -0700

    Updated logic to support recurring events (index by subject instead of by day). Just need to add calendar conversion then Tritonlink is done!

--- content_scripts/tritonlink.js
+++ content_scripts/tritonlink.js
@@ -8,52 +8,42 @@ function begin(message) {
     }
 
     // Do the scrape!
+
     let schedule = scrapeSchedule();
 
     console.log(schedule);
+
+    alert("You're schedule has been scraped!");
 }
 
 
-// Define a handful of functions that can scrape the schedule from this page.
 function scrapeSchedule() {
     const csContainer = document.getElementById("class_schedule");
 
     const csTable = csContainer.children[1].children[0];
 
     const csRows = Array.from(csTable.rows);
-    
-    console.log("Got csRows array");
 
-    let schedule = {};
-    let daysMap = {};
+    const daysRow = csRows[0];
+    const courseRows = csRows.slice(1);
 
-    // Initialize days of schedule as empty lists and map
-    const daysOfWeek = Array.from(csRows[0].cells);
+    let daysMap = initDaysMap(daysRow);
 
-    daysOfWeek.forEach((value, index) => {
-        day = value.textContent.trim();
+    let courses = {};
 
-        schedule[day] = [];
-        daysMap[index] = day;
-    });
-
-    console.log("Initialized schedule and map");
-    console.log(schedule);
-    console.log(daysMap);
 
     /* For each row of courses
         For each day in row
         Trim whitespace, split at newlines, trim whitespace again
         Assign first line to Time, second to Subject, third to Location
+        Associate course info to subject
     */
-    for (let courseRow of csRows.slice(1)) {
-        let courseDays = Array.from(courseRow.children);
+    for (let row of courseRows) {
+        let columns = Array.from(row.children);
 
-        console.log("Converted row into array");
-
-        courseDays.forEach((courseDay, index) => {
+        columns.forEach((column, index) => {
             // Clean the text
-            let courseText = courseDay.textContent;
+            let courseText = column.textContent;
             let splitText = courseText.trim().split('\n').map(Function.prototype.call, String.prototype.trim);
 
             // Parse the information
@@ -66,30 +56,51 @@ function scrapeSchedule() {
 
             let courseLocation = splitText[2];
 
-            let course = {
+            let courseDay = daysMap[index];
+
+            let courseInfo = {
                 startTime: courseStartTime,
                 endTime: courseEndTime,
-                subject: courseSubject,
-                location: courseLocation
+                location: courseLocation,
+                days: [courseDay]
             };
 
-            // Finally add to appropriate day
-            schedule[daysMap[index]].push(course);
+            if (courseSubject) {
 
-// TODO: =======================================================================
-//! SHOULD ASSOCIATE EACH CLASS TO ITS OWN OBJECT WITH DAYS, TIME, LOCATION
-//! TO ALLOW FOR RECURRING EVENTS        
+                if (!courses[courseSubject]) {
+                    // If not yet added to courses, add course info
+                    courses[courseSubject] = courseInfo;
+                } else {
+                    // Otherwise, append the day to list of days
+                    courses[courseSubject]["days"].push(courseDay);
+                }
 
+            }
 
         });
     }
 
+    return courses;
 
 
     // Send to conversion js
 
 
 
-    return schedule;
 
+}
+
+function initDaysMap(daysRow) {
+    daysMap = [];
+
+    // Initialize days of schedule as empty lists and map
+    const daysOfWeek = Array.from(daysRow.cells);
+
+    daysOfWeek.forEach((value, index) => {
+        day = value.textContent.trim();
+
+        daysMap[index] = day;
+    });
+
+    return daysMap;
 }
